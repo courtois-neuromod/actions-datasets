de170f39b45c71b9d3db2f79b031d4f87f3b6175
bpinsard <basile.pinsard@gmail.com>
2025-06-03 16:43:39 -0400

    prettify

--- tests/deploy/test_deploy.py
+++ tests/deploy/test_deploy.py
@@ -18,14 +18,14 @@ def test_get_submodules(dataset):
 def test_autoenable(dataset):
     siblings = dataset.siblings()
     sibling_names = [sib['name'] for sib in siblings]
-    assert not any('sensitive' in sn for sn in sibling_names), 'Sensitive sibling is autoenabled'
+    assert not any('sensitive' in sn for sn in sibling_names), '🚨 Sensitive sibling is autoenabled'
     #assert any('mri' in sn for sn in sibling_names), 'MRI sibling is not autoenabled'
     ora_siblings = [sib for sib in siblings if
         sib.get('annex-type', None)=='external' and
         sib['annex-externaltype']=='ora' and
         not sib.get('annex-httpalso', None) == 'true']
     ora_siblings_names = list(sib['name'] for sib in ora_siblings)
-    assert len(ora_siblings) == 0, f"ORA remotes {','.join(ora_siblings_names)} are autoenabled"
+    assert len(ora_siblings) == 0, f"💣 ORA remotes {','.join(ora_siblings_names)} are autoenabled"
 
 def test_files_in_remote(dataset):
     ds_repo = dataset.repo
@@ -36,7 +36,7 @@ def test_files_in_remote(dataset):
 
     # check that shared files are listed on the share remote
     for public_sibling in public_siblings:
-        logger.info(f"checking file availability in {public_sibling}")
+        logger.info(f"⏳️ checking file availability in {public_sibling}")
         sibling_wanted = public_sibling.get('annex-wanted')
         if sibling_wanted == 'groupwanted':
             sibling_group = ds_repo.call_annex_oneline(['group', public_sibling['name']])
@@ -46,21 +46,21 @@ def test_files_in_remote(dataset):
         shared_files_missing = list(ds_repo.call_annex_items_([
             'find', '--not', '--metadata', 'distribution-restrictions=*',
             '--not', '--in',  public_sibling['name']] + wanted_opts))
-        assert len(shared_files_missing) == 0, f"Files missing in shared remote { public_sibling['name']}: \n{shared_files_missing}"
+        assert len(shared_files_missing) == 0, f"💥 Files missing in shared remote { public_sibling['name']}: \n{shared_files_missing}"
 
         # check all files are in the shared remote
         fsck_res = ds_repo.fsck(remote=public_sibling['name'], fast=True)
-        assert len(fsck_res) > 0, f"git-annex fsck did not give any result, check that remote exists"
+        assert len(fsck_res) > 0, f"❓️git-annex fsck did not give any result, check that remote exists"
         fsck_fails = [fr for fr in fsck_res if not fr['success']]
         fsck_fail_files = [fr['file'] for fr in fsck_fails]
         fsck_num_fail = len(fsck_fails)
-        assert fsck_num_fail == 0, f"git-annex fsck on {public_sibling['name']} failed example error:{fsck_fails[0]} for files: {fsck_fail_files}, "
+        assert fsck_num_fail == 0, f"💥 git-annex fsck on {public_sibling['name']} failed example error:{fsck_fails[0]} for files: {fsck_fail_files}, "
 
         # check that sensitive files are not in the shared remote
         sensitive_files_shared = list(ds_repo.call_annex_items_([
             'find', '--metadata', 'distribution-restrictions=*',
             '--in', public_sibling['name']]))
-        assert len(sensitive_files_shared) == 0, f"Sensitive files mistakenly shared: \n{sensitive_files_shared}"
+        assert len(sensitive_files_shared) == 0, f"🚨 Sensitive files mistakenly shared: \n{sensitive_files_shared}"
 
 
 def get_public_siblings(dataset):
@@ -69,5 +69,5 @@ def get_public_siblings(dataset):
     #sibling_names = [sib['name'] for sib in public_siblings]
     #mri_siblings = [sn for sn in sibling_names if sn.split('.')[-1] == 'mri']
     if len(dataset.repo.get_annexed_files()) > 0:
-        assert len(public_siblings) > 0 , 'at least 1 public remote is required'
+        assert len(public_siblings) > 0 , '❓️at least 1 public remote is required'
     return public_siblings
